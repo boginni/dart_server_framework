0fe5147c8ea1fc45245e2a3aa1fb8ebb5683e509
boginni <brunnomarqueszr1@gmail.com>
2024-02-23 13:09:15 -0300

    first commit

---
+++ bin/dart_server_framework.dart
@@ -0,0 +1,5 @@
+import 'package:dart_server_framework/dart_server_framework.dart' as dart_server_framework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_server_framework.calculate()}!');
+}
---
+++ test/dart_server_framework_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_server_framework/dart_server_framework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
